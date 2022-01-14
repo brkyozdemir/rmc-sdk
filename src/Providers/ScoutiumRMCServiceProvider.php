62995152ec3feeb4526cb8ef9f1d4ec872b6ed25
brkyozdemir <berkayozdemir02@gmail.com>
2022-01-14 11:27:08 +0300

    upadte

--- src/Providers/ScoutiumRMCServiceProvider.php
+++ src/Providers/ScoutiumRMCServiceProvider.php
@@ -8,8 +8,6 @@ class ScoutiumRMCServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/config/rmc.php' => config_path('rmc.php'),
-        ]);
+
     }
 }
